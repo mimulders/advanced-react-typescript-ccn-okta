2c188c35cae616d73674a0ccac66f0285e00163d
mimulders <miriam.mulders@gmail.com>
2024-03-11 15:05:30 +0100

    add layout and page fixtures

---
+++ src/pages/auth/LoginPage.tsx
@@ -0,0 +1,15 @@
+// src/pages/auth/LoginPage.tsx
+import React from "react";
+
+import { Typography, Container } from "@mui/material";
+
+export default function LoginPage() {
+  return (
+    <Container fixed>
+      <Typography variant="h3" component="h1">
+        Codaisseur Coders Network
+      </Typography>
+      <p>Log in</p>
+    </Container>
+  );
+}
---
+++ src/pages/auth/SignupPage.tsx
@@ -0,0 +1,15 @@
+// src/pages/auth/SignUpPage.tsx
+import React from "react";
+
+import { Typography, Container } from "@mui/material";
+
+export default function SignUpPage() {
+  return (
+    <Container fixed>
+      <Typography variant="h3" component="h1">
+        Codaisseur Coders Network
+      </Typography>
+      <p>Sign Up</p>
+    </Container>
+  );
+}
