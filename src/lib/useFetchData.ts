391cc2fb304e38b93ed9d2816d6d6b3d142ca703
mimulders <miriam.mulders@gmail.com>
2024-03-22 12:07:09 +0100

    fix path to base URL

--- src/lib/useFetchData.ts
+++ src/lib/useFetchData.ts
@@ -1,6 +1,6 @@
 // src/lib/useFetchData.ts
 import { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "../axios"; //import the axios instance from the axios.ts file; it contains the base url
 
 import { FetchState } from "../util/fetchstate";
 
@@ -13,7 +13,7 @@ export default function useFetchData<Data>(url: string) {
     (async () => {
       setState({ status: "loading" });
       try {
-        const res = await axios.get(url);
+        const res = await axios.get(url); //url will be appended to the base url from the axios.ts file
         setState({ status: "success", data: res.data });
       } catch (error) {
         setState({ status: "error", error });
